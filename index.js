039657fdcab5cfc2f495680214414491bc16f540
Greyson Harris <gharris@esri.com>
2018-12-20 01:36:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!')
 })
 
-const input = document.querySelector('input')
+let input = document.querySelector('input')
+input.addEventListener('keydown', e => console.log(e.which))
+
+let input = document.querySelector('input')
 input.addEventListener('keydown', e => console.log(e.which))
